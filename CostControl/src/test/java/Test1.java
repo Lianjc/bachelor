e197858a1011844e98a1d153fd596fad4fabf4bb
lianjc <lianjc1025@gmail.com>
2016-03-11 21:53:05 +0800

    [Log] 	1.Edit task book. 	2.Add proposal.

--- CostControl/src/test/java/Test1.java
+++ CostControl/src/test/java/Test1.java
@@ -4,6 +4,8 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -11,17 +13,21 @@ import java.io.Reader;
 /**
  * Created by lianjiangchao on 16/3/10.
  */
-public class Test {
-    public static void main(String[] args) throws IOException{
+public class Test1 {
+
+
+    @Test(enabled = false)
+    public void testMybatis() throws IOException {
         String resource = "mybatisConf/MybatisConf.xml";
         Reader reader = Resources.getResourceAsReader(resource);
         SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
         SqlSession session = sessionFactory.openSession();
-//        String statement = "mapping.UserMapper.getUser";
-//        User user = session.selectOne(statement , 1);
         UserMapper userMapper = session.getMapper(UserMapper.class);
         User user = userMapper.selectByName("lianjc");
-        System.out.print(user);
 
+
+        Assert.assertNotNull(user, "Failed in getting the expected user! NullPointer!");
+        Assert.assertEquals("lianjc@outlook.com", user.getEmail(), "Failed get info!");
     }
+
 }
